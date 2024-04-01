1df382a58dcb6100f6cf70a129316fb3b3ea027d
Roby <robysetiawan2409@gmail.com>
2024-04-01 09:21:47 +0700

    feat: default seo configuration

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,8 @@ import { Router } from 'next/router';
 import NProgress from 'nprogress';
 import '@src/styles/globals.css';
 import { ThemeProvider } from '@src/context/ThemeContext';
+import config from 'next-seo.config';
+import { DefaultSeo } from 'next-seo';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -13,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <main>
+        <DefaultSeo {...config} />
         <Component {...pageProps} />
         <SpeedInsights />
       </main>
