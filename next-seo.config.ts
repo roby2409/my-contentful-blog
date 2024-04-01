b26abde2e4b06e7ec3f4bcb727b8f94f28840ab1
Roby <51867650+roby2409@users.noreply.github.com>
2024-04-01 09:49:27 +0700

    feature/whatsapp and readjust structure folder (#11)
    * refactor: remove unused components
    
    * refactor: remove favicon ico useless
    
    * refactor: change go public robot txt false setting
    
    * refactor: update favicon ico
    
    * feat: default seo configuration
    
    * feat: whatsapp chat at footer

---
+++ next-seo.config.ts
@@ -0,0 +1,31 @@
+import { DefaultSeoProps } from "next-seo";
+
+const config: DefaultSeoProps = {
+  description: 'Software Developer and Frontend Enthusiast that deliver user-centric interfaces for web and mobile applications that exceed client expectations',
+  defaultTitle: 'Roby Setiawan (Software Developer and Frontend Enthusiast)',
+  canonical: 'https://robysetiawan.vercel.app',
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: 'https://robysetiawan.vercel.app/favicon.ico'
+    }
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://robysetiawan.vercel.app',
+    title: 'Roby Setiawan (Software Developer and Frontend Enthusiast)',
+    siteName: 'Roby Setiawan (Software Developer and Frontend Enthusiast)',
+    description: 'Software Developer and Frontend Enthusiast that deliver user-centric interfaces for web and mobile applications that exceed client expectations',
+    images: [
+      {
+        url: 'https://robysetiawan.vercel.app/static/home-image-preview.jpg',
+        alt: 'Islem Maboud Portfolio',
+        type: 'image/jpg',
+        secureUrl: 'https://robysetiawan.vercel.app/static/home-image-preview.jpg'
+      }
+    ]
+  }
+}
+
+export default config;
